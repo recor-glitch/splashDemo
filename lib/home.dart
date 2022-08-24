3045044b15e90af79264d438ffb2e447b78bef46
root <recorglitch@gmail.com>
2022-08-24 15:40:02 +0530

    First splash push.

---
+++ lib/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.network(
+                'https://assets3.lottiefiles.com/packages/lf20_pghdouhq.json')
+          ],
+        ),
+      ),
+    );
+  }
+}
